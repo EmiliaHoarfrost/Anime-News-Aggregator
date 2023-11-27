665e93c77ae27c079dca7b596bcb1bb5a4a0d1c3
Émilia Hoarfrost <125922250+EmiliaHoarfrost@users.noreply.github.com>
2023-11-27 20:32:31 +0100

    Update script.js
    the yuri empire too

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ const feedUrls = [
     "https://artistunknown.info/feed/",
     "https://animetudes.com/feed/",
     "https://keyframe.blog/feed/",
-    "https://www.animefeminist.com/feed/"
+    "https://www.animefeminist.com/feed/,
+    "https://yuriempire.wordpress.com/feed/""
 ];
 
 async function fetchNews() {
